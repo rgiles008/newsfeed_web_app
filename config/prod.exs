2c9af69d8f169cf75c5fbd113dd2f7f6f2be750e
Robert <robert.giles@podium.com>
2020-01-10 20:12:33 -0700

    heroku stuff

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,19 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rb_app, RbAppWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  # url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  url: [scheme: "https", host: "bbnewsfeed.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :rb_app, RbApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -66,4 +76,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
--- lib/rb_app_web/endpoint.ex
+++ lib/rb_app_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule RbAppWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :rb_app
 
   socket "/socket", RbAppWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
